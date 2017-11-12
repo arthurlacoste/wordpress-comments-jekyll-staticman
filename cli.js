077e86157efead9da0ab01324294261909d81487
Arthur Lacoste <arthurlacoste@users.noreply.github.com>
2017-11-12 12:28:33 +0100

    First commit

---
+++ cli.js
@@ -0,0 +1,82 @@
+const fs = require('fs');
+const path = require('path');
+const md5 = require('md5');
+const xml2js = require('xml2js');
+const yaml = require('js-yaml');
+const fspath = require('fs-path');
+
+const args = process.argv.slice(2);
+const parser = new xml2js.Parser();
+let i = 0;
+
+console.log('Starting to find comments...');
+
+const createFile = c => {
+	const timestamp = new Date(c.date).getTime();
+	const filename = path.join(args[1], '/comments/', c.slug,
+  '/comment-', timestamp + '.md');
+	const fileInput = yaml.safeDump(c);
+
+	fspath.writeFile(path.resolve(filename), fileInput, err => {
+		i += 1;
+		console.log(`Adding comment ${i}, by ${c.name}...`);
+		if (err) {
+			throw err;
+		}
+		console.log('Done.');
+	});
+};
+
+const parseComments = (comments, slug) => {
+	for (const index in comments) {
+		if (Object.prototype.hasOwnProperty.call(comments, index)) {
+			const comment = comments[index];
+
+			if (comment['wp:comment_approved'][0] === '1') {
+				const commentObj = {
+					slug,
+					name: comment['wp:comment_author'][0],
+					date: comment['wp:comment_date'][0],
+					url: comment['wp:comment_author_url'][0],
+					message: comment['wp:comment_content'][0],
+					email: md5(comment['wp:comment_author_email'][0])
+				};
+
+				createFile(commentObj);
+        /* Filter by length ?
+        if(commentObj.message.length<50) {
+          console.log(commentObj)
+          i += 1
+          console.log('I', i)
+
+        } */
+			}
+		}
+	}
+};
+
+fs.readFile(path.resolve(args[0]), (err, data) => {
+	if (err) {
+		throw err;
+	}
+	parser.parseString(data, (err, result) => {
+		if (err) {
+			throw err;
+		}
+
+		const posts = result.rss.channel[0].item;
+
+		for (const index in posts) {
+			if (Object.prototype.hasOwnProperty.call(posts, index)) {
+				const post = posts[index];
+
+				if (post['wp:comment']) {
+					const slug = post['wp:post_name'][0];
+					const comments = post['wp:comment'];
+
+					parseComments(comments, slug);
+				}
+			}
+		}
+	});
+});
---
+++ readme.md
@@ -0,0 +1,56 @@
+# wordpress-comments-jekyll-staticman
+
+[![NPM Version][npm-version]][npm-url]
+[![travis][travis-badge]][travis-url]
+[![xo][xo-badge]][xo-url]
+
+This little import tool allow to imports comments from a WordPress blog, to be used on [Jekyll] with [Staticman] (v2).
+
+## Install
+
+```
+npm i wordpress-comments-jekyll-staticman -g
+```
+
+## Usage
+
+First, import your comments from WordPress with the built-in export tool, then launch this command:
+
+```terminal
+wordpress-comments-jekyll-staticman {{ xml file }} {{ folder for comments }}
+```
+
+Example:
+
+```terminal
+wordpress-comments-jekyll-staticman comments.xml /my/folder
+```
+
+## Staticman settings
+
+Theses settings are used on the app, and was made to work with [this kind of themes].
+
+You can paste this fields to your `staticman.yml` file:
+
+```yml
+staticman:
+  allowedFields          : ['name', 'email', 'url', 'message']
+  filename               : comment-{@timestamp}
+  format                 : "yml"
+  path                   : "/_data/comments/{options.slug}"
+```
+
+# Licence
+
+MIT
+
+[npm-version]:https://img.shields.io/npm/v/wordpress-comments-jekyll-staticman.svg
+[npm-url]: https://npmjs.org/package/wordpress-comments-jekyll-staticman
+[travis-badge]: http://img.shields.io/travis/arthurlacoste/wordpress-comments-jekyll-staticman.svg
+[travis-url]: https://travis-ci.org/arthurlacoste/wordpress-comments-jekyll-staticman
+[xo-badge]: https://img.shields.io/badge/code_style-XO-5ed9c7.svg
+[xo-url]: https://github.com/sindresorhus/xo
+
+[Jekyll]: https://jekyllrb.com
+[Staticman]: https://staticman.net
+[this kind of themes]: https://mademistakes.com/work/minimal-mistakes-jekyll-theme/
